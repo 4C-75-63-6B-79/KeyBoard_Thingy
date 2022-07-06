214a79f0fd825d8a570c434c1eb8ab4c74339094
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-07-06 15:33:09 +0530

    All files are created and doen

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # KeyBoard_Thingy
+when a alphabet key is pressed on the keyboard the key is also pressed on the keyborard on screen.
---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+
+window.addEventListener('keydown', keyPressed);
+const keys = Array.from(document.querySelectorAll('.button'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function keyPressed(event) {
+    // console.log(event.code);
+    const key = document.querySelector(`[data-key="${event.code}"]`);
+    if(key !== null) {
+        // console.log(key);
+        key.classList.add('button-pressed');
+    }
+}
+
+function removeTransition(event) {
+    if(event.propertyName == "transform") {
+        event.target.classList.remove('button-pressed');
+    }
+}
